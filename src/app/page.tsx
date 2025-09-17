5d82a999b7a2c1722dfa2732bf60d159b8266dde
SpeedyxBR <andrel.cilva57@gmail.com>
2025-09-16 21:31:38 -0300

    feat: add Header component and Banner

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,33 @@
-import Header from "@/app/_components/header"
+import { Button } from "./_components/ui/button"
+import { SearchIcon } from "lucide-react"
+import Header from "./_components/header"
+import { Input } from "./_components/ui/input"
+import Image from "next/image"
 
 const Home = () => {
   return (
     <div>
       <Header />
+      <div className="p-5">
+        <h2 className="text-xl font-bold">Olá. Andrel!</h2>
+        <p>Segunda-feira, 16 de Setembro.</p>
+
+        <div className="mt-6 flex items-center gap-8">
+          <Input placeholder="Faça sua busca..." />
+          <Button>
+            <SearchIcon />
+          </Button>
+        </div>
+
+        <div className="relative mt-6 h-[150px] w-full overflow-hidden rounded-lg">
+          <Image
+            alt="Agende nos melhores com FSW Barber"
+            src="/banner-01.png"
+            fill
+            className="object-cover"
+          />
+        </div>
+      </div>
     </div>
   )
 }
