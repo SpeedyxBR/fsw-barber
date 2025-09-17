93d2a4bf08c16bf0e0dd558cf11ed7f7b7c2c8a6
SpeedyxBR <andrel.cilva57@gmail.com>
2025-09-17 00:56:26 -0300

    feat: add bagde in page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,9 @@ import { SearchIcon } from "lucide-react"
 import Header from "./_components/header"
 import { Input } from "./_components/ui/input"
 import Image from "next/image"
+import { CardContent, Card } from "./_components/ui/card"
+import { Badge } from "./_components/ui/badge"
+import { Avatar, AvatarImage } from "./_components/ui/avatar"
 
 const Home = () => {
   return (
@@ -12,13 +15,14 @@ const Home = () => {
         <h2 className="text-xl font-bold">Olá. Andrel!</h2>
         <p>Segunda-feira, 16 de Setembro.</p>
 
+        {/* BUSCA */}
         <div className="mt-6 flex items-center gap-8">
           <Input placeholder="Faça sua busca..." />
           <Button>
             <SearchIcon />
           </Button>
         </div>
-
+        {/* IMAGEM */}
         <div className="relative mt-6 h-[150px] w-full overflow-hidden rounded-lg">
           <Image
             alt="Agende nos melhores com FSW Barber"
@@ -27,6 +31,24 @@ const Home = () => {
             className="object-cover"
           />
         </div>
+
+        {/* AGENDAMENTO */}
+        <Card className="mt-6">
+          <CardContent className="flex">
+            {/* ESQUERDA */}
+            <div className="flex flex-col gap-2 py-5">
+              <Badge className="w-fit">Confirmado</Badge>
+              <h3 className="fonte font-semibold">Corte de Cabelo</h3>
+
+              <div className="flex items-center gap-2">
+                <Avatar className="h-6 w-6">
+                  <AvatarImage src="https://utfs.io/f/c97a2dc9-cf62-468b-a851-bfd2bdde775f-16p.png" />
+                </Avatar>
+                <p className="text-sm">Barbearia FSW</p>
+              </div>
+            </div>
+          </CardContent>
+        </Card>
       </div>
     </div>
   )
