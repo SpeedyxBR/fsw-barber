42367493777be33ba0f921a5867683c92cf11661
SpeedyxBR <andrel.cilva57@gmail.com>
2025-09-18 16:01:13 -0300

    feat: add BarbershopItem component and disable lint-staged

--- src/app/_components/barbershop.item.tsx
+++ src/app/_components/barbershop.item.tsx
@@ -1,23 +1,37 @@
-import { Barbershop } from "@prisma/client"
+import { BarberShop } from "@prisma/client"
 import { Card, CardContent } from "./ui/card"
 import Image from "next/image"
+import { Button } from "./ui/button"
+import { StarIcon } from "lucide-react"
 
 interface BarbershopItemProps {
-  barbershop: Barbershop
+  barbershop: BarberShop
 }
 
 const BarbershopItem = ({ barbershop }: BarbershopItemProps) => {
   return (
-    <Card className="min-w-[159px]">
-      <CardContent className="p-0">
+    <Card className="min-w-[167px] rounded-2xl">
+      <CardContent className="p-0 px-1 pt-1">
         {/* IMAGEM */}
-        <div className="relative h-[159px] w-[159px] w-full">
+        <div className="relative h-[159px] w-[159px]">
           <Image
             alt={barbershop.name}
             fill
-            className="object-contain object-cover"
+            className="rounded-2xl object-cover"
             src={barbershop.imageUrl}
           />
+          <div className="absolute left-2 top-2 z-10 flex items-center gap-1 rounded-full bg-gray-900 px-2 py-1 opacity-90">
+            <StarIcon size={12} className="fill-primary text-primary" />
+            <span className="text-xs font-bold text-white">5,0</span>
+          </div>
+        </div>
+        {/* TEXTO */}
+        <div className="px-1 py-3">
+          <h3 className="truncate font-semibold">{barbershop.name}</h3>
+          <p className="truncate text-sm text-gray-400">{barbershop.address}</p>
+          <Button variant="secondary" className="mt-3 w-full">
+            Reservar
+          </Button>
         </div>
       </CardContent>
     </Card>
