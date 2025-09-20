6a266b78a8eae85a39f97e8dcdef4d8adfa831c8
SpeedyxBR <andrel.cilva57@gmail.com>
2025-09-19 23:03:14 -0300

    feat: refactor BookingItem to include card structure and booking information

--- src/app/_components/booking-item.tsx
+++ src/app/_components/booking-item.tsx
@@ -1,27 +1,31 @@
-const  = () => {
-    return (  );
+import { Card, CardContent } from "@/app/_components/ui/card"
+import { Badge } from "@/app/_components/ui/badge"
+import { Avatar, AvatarImage } from "@/app/_components/ui/avatar"
+
+const BookingItem = () => {
+  return (
+    <Card>
+      <CardContent className="flex justify-between p-0">
+        {/* ESQUERDA */}
+        <div className="flex flex-col gap-2 py-5 pl-5">
+          <Badge className="w-fit">Confirmado</Badge>
+          <h3 className="fonte font-semibold">Corte de Cabelo</h3>
+          <div className="flex items-center gap-2">
+            <Avatar className="h-6 w-6">
+              <AvatarImage src="https://utfs.io/f/c97a2dc9-cf62-468b-a851-bfd2bdde775f-16p.png" />
+            </Avatar>
+            <p className="text-sm">Barbearia FSW</p>
+          </div>
+        </div>
+        {/* Direita */}
+        <div className="flex flex-col items-center justify-center border-l-2 border-solid px-5">
+          <p className="text-sm">Agosto</p>
+          <p className="text-2xl">05</p>
+          <p className="text-sm">14:00</p>
+        </div>
+      </CardContent>
+    </Card>
+  )
 }
- 
-export default ;
- </h2>
-        <Card>
-          <CardContent className="flex justify-between p-0">
-            {/* ESQUERDA */}
-            <div className="flex flex-col gap-2 py-5 pl-5">
-              <Badge className="w-fit">Confirmado</Badge>
-              <h3 className="fonte font-semibold">Corte de Cabelo</h3>
-              <div className="flex items-center gap-2">
-                <Avatar className="h-6 w-6">
-                  <AvatarImage src="https://utfs.io/f/c97a2dc9-cf62-468b-a851-bfd2bdde775f-16p.png" />
-                </Avatar>
-                <p className="text-sm">Barbearia FSW</p>
-              </div>
-            </div>
-            {/* Direita */}
-            <div className="flex flex-col items-center justify-center border-l-2 border-solid px-5">
-              <p className="text-sm">Agosto</p>
-              <p className="text-2xl">05</p>
-              <p className="text-sm">14:00</p>
-            </div>
-          </CardContent>
-        </Card>
+
+export default BookingItem
